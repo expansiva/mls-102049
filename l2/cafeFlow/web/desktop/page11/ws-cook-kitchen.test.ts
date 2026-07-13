9908a28a3e9220947e6c6f6e6c98630893add089
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-07-13 16:37:04 -0300

    del

--- l2/cafeFlow/web/desktop/page11/ws-cook-kitchen.test.ts
+++
@@ -1,2 +0,0 @@
-/// <mls fileReference="_102049_/l2/cafeFlow/web/desktop/page11/ws-cook-kitchen.test.ts" enhancement="_blank"/>
-
--- l2/cafeFlow/web/desktop/page11/ws-manager-menu.test.ts
+++
@@ -1,2 +0,0 @@
-/// <mls fileReference="_102049_/l2/cafeFlow/web/desktop/page11/ws-manager-menu.test.ts" enhancement="_blank"/>
-
