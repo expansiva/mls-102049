a7f1c420f5bd25fd2a68f6759e2376369720eee4
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-07-16 18:16:19 -0300

    del

--- l1/petShop/layer_1_external/adapters/persistence/registerRepositories.ts
+++
@@ -1,7 +0,0 @@
-/// <mls fileReference="_102049_/l1/petShop/layer_1_external/adapters/persistence/registerRepositories.ts" enhancement="_blank"/>
-import { registerRepository } from '/_102034_/l1/server/layer_2_application/repositoryRegistry.js';
-import { createAdoptablePetRepositoryAdapter } from '/_102049_/l1/petShop/layer_1_external/adapters/persistence/adoptablePetRepositoryAdapter.js';
-import { createProductRepositoryAdapter } from '/_102049_/l1/petShop/layer_1_external/adapters/persistence/productRepositoryAdapter.js';
-
-registerRepository('AdoptablePet', createAdoptablePetRepositoryAdapter);
-registerRepository('Product', createProductRepositoryAdapter);
